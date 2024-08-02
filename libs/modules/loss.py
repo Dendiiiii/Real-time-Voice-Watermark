correct perceptual loss function calculation

libs/modules/loss.py
@@ -8,6 +8,7 @@ import torchaudio.functional as F
 
 # Approximated Fletcher-Munson curve
 def fletcher_munson_weights(freqs):
+    freqs = np.maximum(freqs, 1e-6)
     return 3.64 * (freqs/1000)**-0.8 - 6.5 * np.exp(-0.6 * (freqs/1000 - 3.3)**2) + (10**-3) * (freqs/1000)**4
 
 
@@ -33,31 +34,31 @@ def perceptual_loss(watermark, sample_rate=16000):
     return loss
 
 
-class TFLoudnessLoss(nn.Module):
-    def __init__(self):
-        super(TFLoudnessLoss, self).__init__()
-
-    def forward(self, signals, watermarks, sample_rate):
-        meter = pyln.Meter(sample_rate)
-        signal_loudness = [meter.integrated_loudness(signal.cpu().detach().numpy()) for signal in signals]
-        signal_loudness_tensor = torch.tensor(signal_loudness)
-        watermark_loudness = [meter.integrated_loudness(watermark.cpu().detach().numpy()) for watermark in watermarks]
-        watermark_loudness_tensor = torch.tensor(watermark_loudness)
-        loudness_loss = torch.mean(torch.abs(watermark_loudness_tensor - signal_loudness_tensor))
-        return loudness_loss
+# class TFLoudnessLoss(nn.Module):
+#     def __init__(self):
+#         super(TFLoudnessLoss, self).__init__()
+#
+#     def forward(self, signals, watermarks, sample_rate):
+#         meter = pyln.Meter(sample_rate)
+#         signal_loudness = [meter.integrated_loudness(signal.cpu().detach().numpy()) for signal in signals]
+#         signal_loudness_tensor = torch.tensor(signal_loudness)
+#         watermark_loudness = [meter.integrated_loudness(watermark.cpu().detach().numpy()) for watermark in watermarks]
+#         watermark_loudness_tensor = torch.tensor(watermark_loudness)
+#         loudness_loss = torch.mean(torch.abs(watermark_loudness_tensor - signal_loudness_tensor))
+#         return loudness_loss
 
 
 class Loss(nn.Module):
     def __init__(self):
         super(Loss, self).__init__()
         self.bce_loss = nn.BCELoss()
-        self.loudness_loss = TFLoudnessLoss()
+        # self.loudness_loss = TFLoudnessLoss()
         self.l1_loss = nn.L1Loss()
 
     def en_de_loss(self, x, w_x, wm, prob, labels):
         # 从小数点到db level做mse, regularizer
         bce_loss = self.bce_loss(prob[:, 0, :], labels.type(torch.float32))
-        loudness_loss = self.loudness_loss(x, wm, 16000)
+        # loudness_loss = self.loudness_loss(x, wm, 16000)
         l1_loss = self.l1_loss(wm, torch.zeros_like(wm))
         percep_loss = perceptual_loss(wm)
         return l1_loss, bce_loss, percep_loss