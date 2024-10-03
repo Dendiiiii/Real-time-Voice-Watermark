892e057a198461177823f3d1d0ccc44a01cdd588
Yizhu Wen <wenyizhuuk@126.com>
2024-10-02 22:02:40 -1000

    Change the wav length requirement to be greater than 2.55s

--- dataset/data.py
+++ dataset/data.py
@@ -27,38 +27,6 @@ def collate_fn(batch):
     return batched_sample
 
 
-class oned_dataset(Dataset):
-    def __init__(self, process_config, train_config, flag='train'):
-        self.dataset_name = train_config["dataset"]
-        raw_dataset_path = train_config["path"]["raw_path"]
-        self.dataset_path = os.path.join(raw_dataset_path, flag)
-        self.sample_rate = process_config["audio"]["sample_rate"]
-        self.max_wav_value = process_config["audio"]["max_wav_value"]
-        self.win_len = process_config["audio"]["win_len"]
-        self.max_len = process_config["audio"]["max_len"]
-        self.wavs = self.process_meta()
-
-    def __len__(self):
-        return len(self.wavs)
-
-    def __getitem__(self, idx):
-        audio_name = self.wavs[idx]
-        wav, sr = torchaudio.load(os.path.join(self.dataset_path, audio_name))
-        wav = wav[:, :self.max_len]
-        sample = {
-            "matrix": wav,
-            "sample_rate": sr,
-            "patch_num": 0,
-            "pad_num": 0,
-            "name": audio_name
-        }
-        return sample
-
-    def process_meta(self):
-        wavs = os.listdir(self.dataset_path)
-        return wavs
-
-
 class wav_dataset(Dataset):
     def __init__(self, process_config, train_config, flag='train'):
         self.dataset_name = train_config["dataset"]
@@ -73,24 +41,32 @@ class wav_dataset(Dataset):
         sr = process_config["audio"]["or_sample_rate"]
         self.resample = torchaudio.transforms.Resample(sr, self.sample_rate)
         self.sample_list = []
+        # 2.05s * 16000 = 32800 frames
+        # 0.5s * 16000 = 8000 frames
+        # Input audio length needs to be greater than 2.55s (32800+8000 frames)
+        min_length = 32800+8000
+
         for idx in range(len(self.wavs)):
             audio_name = self.wavs[idx]
-            wav, sr = torchaudio.load(os.path.join(self.dataset_path, audio_name))
-            # 2.05s * 16000 = 32800 frames
 
-            if wav.shape[1] > self.max_len:
-                cuted_len = random.randint(5*sr, self.max_len)
-                wav = wav[:, :cuted_len]
-            if sr != self.sample_rate:
-                wav = self.resample(wav[0, :].view(1, -1))
-            sample = {
-                "matrix": wav,
-                "sample_rate": sr,
-                "patch_num": 0,
-                "pad_num": 0,
-                "name": audio_name
-            }
-            self.sample_list.append(sample)
+            wav, sr = torchaudio.load(os.path.join(self.dataset_path, audio_name))
+
+            if wav.shape[1] > min_length:
+                if wav.shape[1] > self.max_len:
+                    cuted_len = random.randint(5*sr, self.max_len)
+                    wav = wav[:, :cuted_len]
+                if sr != self.sample_rate:
+                    wav = self.resample(wav[0, :].view(1, -1))
+                sample = {
+                    "matrix": wav,
+                    "sample_rate": sr,
+                    "patch_num": 0,
+                    "pad_num": 0,
+                    "name": audio_name
+                }
+                self.sample_list.append(sample)
+            else:
+                print("The length of {} is shorter than 2.55s".format(audio_name))
 
     def __len__(self):
         return len(self.sample_list)
@@ -99,4 +75,5 @@ class wav_dataset(Dataset):
         return self.sample_list[idx]
 
     def process_meta(self):
-        return os.listdir(self.dataset_path)
+        wavs = os.listdir(self.dataset_path)
+        return wavs
