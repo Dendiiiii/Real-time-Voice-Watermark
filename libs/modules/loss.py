Fixed the loudness calculation code

libs/modules/loss.py
@@ -88,7 +88,9 @@ class Loss(nn.Module):
         self.l1_loss = nn.L1Loss()
         self.l2_loss = nn.MSELoss()
 
-    def en_de_loss(self, x, w_x, wm, prob, labels, decoded_msg, message, sample_rate=16000):
+    def en_de_loss(
+        self, x, w_x, wm, prob, labels, decoded_msg, message, sample_rate=16000
+    ):
         # 从小数点到db level做mse, regularizer
         bce_loss = self.bce_loss(prob[:, 0, :], labels)
         decode_bce_loss = self.bce_loss(decoded_msg, message)
@@ -100,7 +102,11 @@ class Loss(nn.Module):
         # grad_penalty_loss = gradient_penalty_loss(wm)*0.001
         smoothness_loss = 0  # tvl_loss + grad_penalty_loss
         freq_loss = frequency_domain_loss(x, w_x)
-        loudness_loss = AudioSignal(x, sample_rate).loudness() - AudioSignal(wm, sample_rate).loudness()
+
+        loudness_loss = (
+            AudioSignal(x.unsqueeze(1), sample_rate).loudness()
+            - AudioSignal(wm.unsqueeze(1), sample_rate).loudness()
+        ).mean()
 
         return (
             hybrid_loss_value * 0,
@@ -109,5 +115,5 @@ class Loss(nn.Module):
             smoothness_loss,
             freq_loss * 0,
             decode_bce_loss,
-            loudness_loss
+            loudness_loss,
         )

test.py
@@ -1,7 +1,6 @@
 import os
 
 import soundfile
-import yaml
 from rich.progress import track
 from torch.utils.data import DataLoader
 
@@ -100,12 +99,12 @@ def main(configs):
             )
 
 
-if __name__ == "__main__":
-    process_config = yaml.load(
-        open(r"./config/process.yaml", "r"), Loader=yaml.FullLoader
-    )
-    model_config = yaml.load(open(r"./config/model.yaml", "r"), Loader=yaml.FullLoader)
-    train_config = yaml.load(open(r"./config/train.yaml", "r"), Loader=yaml.FullLoader)
-    configs = (process_config, model_config, train_config)
-    main(configs)
-    torch.cuda.empty_cache()
+# if __name__ == "__main__":
+#     process_config = yaml.load(
+#         open(r"./config/process.yaml", "r"), Loader=yaml.FullLoader
+#     )
+#     model_config = yaml.load(open(r"./config/model.yaml", "r"), Loader=yaml.FullLoader)
+#     train_config = yaml.load(open(r"./config/train.yaml", "r"), Loader=yaml.FullLoader)
+#     configs = (process_config, model_config, train_config)
+#     main(configs)
+#     torch.cuda.empty_cache()

training.py
@@ -4,6 +4,7 @@ from itertools import chain
 
 import matplotlib
 import matplotlib.pyplot as plt
+import torchaudio
 import yaml
 from rich.progress import track
 from torch.optim import AdamW
@@ -14,7 +15,6 @@ from dataset.data import collate_fn
 from dataset.data import wav_dataset as my_dataset
 from Distortions.distortions import *
 from libs.modules.loss import Loss
-from libs.modules.segmentation import *
 from models import *
 
 matplotlib.use("Agg")  # Use a non-interactive backend
@@ -49,14 +49,16 @@ def select_random_chunk(audio_data, percentage=1.0):
         chunk_length = int(total_length * percentage)
 
         # Correct randint usage for selecting a start_point
-        start_point = torch.randint(0, total_length - chunk_length + 1, (1,), device=audio_data.device).item()
+        start_point = torch.randint(
+            0, total_length - chunk_length + 1, (1,), device=audio_data.device
+        ).item()
 
         end_point = start_point + chunk_length
 
         # Create the label vector for selected sequence
         # mask the first 2.05s sample to be zero (unwatermarked)
         # 2.05s * 16000 = 32800
-        label_vector[i, start_point+32800:end_point] = 1
+        label_vector[i, start_point + 32800 : end_point] = 1
 
         # Select the audio using the label vector
         selected_audio = audio_data[i, start_point:end_point]
@@ -90,8 +92,6 @@ def substitute_watermarked_audio(wav_matrix, watermarked_wav, label_vector):
 def main(configs):
     logging.info("main function")
     process_config, model_config, train_config = configs
-    prev_step = 0
-    test_mode = True
 
     # ------------------- get train dataset
     train_audios = my_dataset(
@@ -278,7 +278,7 @@ def main(configs):
                 prob,
                 label_vec,
                 decoded_msg,
-                msg
+                msg,
             )
 
             # Convert probabilities to binary values (0 or 1) using a threshold of 0.5
@@ -292,9 +292,14 @@ def main(configs):
             ber = bit_errors / total_bits
 
             sum_loss = (
-                losses[0] + losses[1] + losses[2] + losses[3] + losses[4] + losses[5] + losses[6]
+                losses[0]
+                + losses[1]
+                + losses[2]
+                + losses[3]
+                + losses[4]
+                + losses[5]
+                + losses[6]
             )
-
             sum_loss.backward()
             en_de_op.step()
 
@@ -312,7 +317,14 @@ def main(configs):
                 logging.info(
                     "training_step:{} - l1_loss:{:.8f} - bce_loss:{:.8f} - "
                     "perceptual_loss:{:.8f} - freq_loss:{:.8f} - decode_bce_loss:{:.8f} - BER:{:.8f} - loudness:{:.8f}".format(
-                        step, losses[0], losses[1], losses[2], losses[4], losses[5], ber, losses[6]
+                        step,
+                        losses[0],
+                        losses[1],
+                        losses[2],
+                        losses[4],
+                        losses[5],
+                        ber,
+                        losses[6],
                     )
                 )
 
@@ -419,7 +431,7 @@ def main(configs):
                     prob,
                     label_vec,
                     decoded_msg,
-                    msg
+                    msg,
                 )
 
                 # Convert probabilities to binary values (0 or 1) using a threshold of 0.5
@@ -450,7 +462,12 @@ def main(configs):
             val_ber = running_ber / len(val_audios_loader)
             val_loudness_loss = running_loudness_loss / len(val_audios_loader)
             val_total_loss = (
-                val_l1_loss + val_bce_loss + val_perceptual_loss + val_freq_loss + val_decode_bce + val_loudness_loss
+                val_l1_loss
+                + val_bce_loss
+                + val_perceptual_loss
+                + val_freq_loss
+                + val_decode_bce
+                + val_loudness_loss
             )
 
             val_metrics = {
@@ -645,7 +662,7 @@ def main(configs):
                 prob,
                 label_vec,
                 decoded_msg,
-                msg
+                msg,
             )
 
             # Convert probabilities to binary values (0 or 1) using a threshold of 0.5
@@ -800,7 +817,12 @@ def main(configs):
         test_ber = running_ber / len(dev_audios_loader)
         test_loudness_loss = running_loudness_loss / len(dev_audios_loader)
         test_total_loss = (
-            test_l1_loss + test_bce_loss + test_perceptual_loss + test_freq_loss + test_decode_bce + test_loudness_loss
+            test_l1_loss
+            + test_bce_loss
+            + test_perceptual_loss
+            + test_freq_loss
+            + test_decode_bce
+            + test_loudness_loss
         )
 
         test_loss_summary_table.add_data(
@@ -811,7 +833,7 @@ def main(configs):
             test_decode_bce,
             test_ber,
             test_loudness_loss,
-            test_total_loss
+            test_total_loss,
         )
 
         wandb.log(